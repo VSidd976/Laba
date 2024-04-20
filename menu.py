df60a600f28098a7976659fe4a4f8d7136abf3b1
Merdet <40603017+Merdet@users.noreply.github.com>
2024-04-20 19:47:15 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -25,27 +25,3 @@ class Menu:
         if self.index == len(self.content):
             self.index = 0
 
-
-menu1 = Menu("Mohyla", ["People", "Structures"])
-menu2 = Menu("People", ["Students", "Lecturers"])
-menu3 = Menu("Structures", ["Cathedras", "Faculties"])
-menu4 = Menu("Cathedras", ["Math", "Economics", "Biology"])
-menu5 = Menu("Faculties", ["Fi", "Fen", "Fprn"])
-menu6 = Menu("Students", ["Dima", "Valentin"])
-menu7 = Menu("Lecturers", ["Kozir", "Mitnyk"])
-menu_dict = {"Mohyla": menu1, "People": menu2,
-             "Structures": menu3, "Cathedras": menu4,
-             "Faculties": menu5, "Students": menu6,
-             "Lecturers": menu7}
-main_menu = menu1
-while True:
-    print(main_menu.display())
-    command = input("Enter command: Up Down Open Exit ")
-    if command.lower() == "u":
-        main_menu.up()
-    elif command.lower() == "d":
-        main_menu.down()
-    elif command.lower() == "o":
-        main_menu = menu_dict[main_menu.content[main_menu.index]]
-    else:
-        break
