a572c3324d7b337f19ddc2d1f046bcdba1699d74
Merdet <40603017+Merdet@users.noreply.github.com>
2024-04-20 14:33:07 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
+
 class Menu:
-    def __init__(self, name, content):
+    def __init__(self, name: str, content: list):
         self.name = name
         self.content = content
         self.index = 0
@@ -24,29 +25,31 @@ class Menu:
         if self.index == len(self.content):
             self.index = 0
 
-    @staticmethod
-    def use(name, content):
-        new_menu = Menu(name, content)
-        return new_menu.display()
-# dick = {"Mohyla": ["People", "Structures", "Exit"]
-#       "People": ["Students","Lecturers"]
-#      "Structures": ["Cathedra", "Faculties"]}
-
-
-name_of_university = "Mohyla"
-content_of_university = ["People", "Structures", "Exit"]
-menu = Menu(name_of_university, content_of_university)
-print(menu.display())
-menu.up()
-print(menu.display())
-menu.up()
-print(menu.display())
-menu.down()
-print(menu.display())
-menu.down()
-print(menu.display())
+menu1 = Menu("Mohyla",["People", "Structures"])
 menu2 = Menu("People", ["Students", "Lecturers"])
-menu3 = Menu("Structures", ["Cathedra", "Faculties"])
-print(menu2.display())
-print(menu3.display())
-print(menu.use("People", ["Students"]))
+menu3 = Menu("Structures", ["Cathedras", "Faculties"])
+menu4 = Menu("Cathedras", ["Math", "Economics", "Biology"])
+menu5 = Menu("Faculties", ["Fi", "Fen", "Fprn"])
+menu6 = Menu("Students",["Dima", "Valentin"])
+menu7 = Menu("Lecturers", ["Kozir", "Mitnyk"])
+menu_dict = {"Mohyla": menu1, "People": menu2,
+             "Structures": menu3, "Cathedras": menu4,
+             "Faculties": menu5, "Students": menu6,
+             "Lecturers": menu7}
+main_menu = menu1
+while True:
+    print(main_menu.display())
+    command = input("Enter command: Up Down Open Exit ")
+    if command.lower() == "u":
+        main_menu.up()
+    elif command.lower() == "d":
+        main_menu.down()
+    elif command.lower() == "o":
+        main_menu = menu_dict[main_menu.content[main_menu.index]]
+    else:
+        break
+
+
+
+
+
